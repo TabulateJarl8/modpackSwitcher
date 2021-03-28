25341d287d70634580ce5bd6e27e0df59cc0884e
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-03-28 02:32:41 -0400

    fix debug flag

--- src/main/java/modpackSwitcher/Main.java
+++ src/main/java/modpackSwitcher/Main.java
@@ -18,6 +18,7 @@ public class Main {
         Main main = new Main();
 
         boolean ansi = true;
+        boolean debug = false;
         for (String arg : args) {
             switch (arg) {
                 case "--no-ansi":
@@ -37,48 +38,15 @@ public class Main {
                     System.exit(0);
                 case "--debug":
                 case "-d":
-                    System.out.println("--- MODPACKSWITCHER DEBUG START ---\n");
-
-                    System.out.println("OS: " + System.getProperty("os.name") + " | " + System.getProperty("os.version") + " | " + System.getProperty("os.arch") + "\n");
-
-                    String absolutePath = FileSystems.getDefault().getPath("packs").normalize().toAbsolutePath().toString();
-                    System.out.println("packs directory in current directory: " + Files.exists(Paths.get(absolutePath)));
-                    System.out.println("packs directory path: " + absolutePath);
-
-                    File packsDir = new File(absolutePath);
-                    String[] modpacks = packsDir.list(new FilenameFilter() {
-                        @Override
-                        public boolean accept(File file, String s) {
-                            return new File(file, s).isDirectory();
-                        }
-                    });
-                    System.out.println("packs directory contents: " + Arrays.toString(modpacks) + "\n");
-
-                    System.out.print("Java: ");
-                    try {
-                        Runtime rt = Runtime.getRuntime();
-                        Process proc = rt.exec("java -version");
-                        int exitVal = proc.waitFor();
-                        if (exitVal == 0) {
-                            System.out.println("yes");
-                            System.out.println("Version: " + System.getProperty("java.version"));
-                        } else {
-                            System.out.println("no");
-                        }
-                    } catch (IOException | InterruptedException e) {
-                        System.out.println("no (IOException | InterruptedException)");
-                    }
-
-                    System.out.println("\n--- MODPACKSWITCHER DEBUG END ---");
-                    System.exit(0);
+                    debug = true;
             }
         }
 
-        main.run(ansi);
+        main.run(ansi, debug);
 
         }
 
-    public void run(boolean ansi) {
+    public void run(boolean ansi, boolean debug) {
 
         String absolutePath;
         String[] modpacks;
@@ -111,6 +79,43 @@ public class Main {
                 System.out.println(ansi ? Fore.YELLOW + "Warning:" + Fore.RESET + " Error on reading config file at " + Fore.WHITE + "\"" + file.getAbsoluteFile() + "\"" + Fore.RESET : "Warning: Error on reading config file at \"./mpswconfig.ini\"");
                 lastUsedPackString = "0";
             }
+        }
+        
+        if (debug) {
+        	System.out.println("--- MODPACKSWITCHER DEBUG START ---\n");
+
+                    System.out.println("OS: " + System.getProperty("os.name") + " | " + System.getProperty("os.version") + " | " + System.getProperty("os.arch") + "\n");
+
+                    String absolutePath = config.get("system", "absoluteworkingdir") + "/packs";
+                    System.out.println("packs directory in current directory: " + Files.exists(Paths.get(absolutePath)));
+                    System.out.println("packs directory path: " + absolutePath);
+
+                    File packsDir = new File(absolutePath);
+                    String[] modpacks = packsDir.list(new FilenameFilter() {
+                        @Override
+                        public boolean accept(File file, String s) {
+                            return new File(file, s).isDirectory();
+                        }
+                    });
+                    System.out.println("packs directory contents: " + Arrays.toString(modpacks) + "\n");
+
+                    System.out.print("Java: ");
+                    try {
+                        Runtime rt = Runtime.getRuntime();
+                        Process proc = rt.exec("java -version");
+                        int exitVal = proc.waitFor();
+                        if (exitVal == 0) {
+                            System.out.println("yes");
+                            System.out.println("Version: " + System.getProperty("java.version"));
+                        } else {
+                            System.out.println("no");
+                        }
+                    } catch (IOException | InterruptedException e) {
+                        System.out.println("no (IOException | InterruptedException)");
+                    }
+
+                    System.out.println("\n--- MODPACKSWITCHER DEBUG END ---");
+                    System.exit(0);
         }
 
         absolutePath = config.get("system", "absoluteworkingdir") + "/packs";
